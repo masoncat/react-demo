a36c1e8e49b65d11bd1dbef46f7e4ead7863e6f2
= <=>
2018-02-05 16:44:53 +0800

    update

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,27 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 module.exports = merge(common, {
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: [
+                {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['react', 'es2015']
+                    }
+                }
+            ]
+        },{
+            test: /\.scss$/,
+            exclude: /node_modules/,
+            loader: [
+                'style-loader',
+                'css-loader',
+            ]
+        }]
+    },
     plugins: [
         new UglifyJSPlugin()
     ]
